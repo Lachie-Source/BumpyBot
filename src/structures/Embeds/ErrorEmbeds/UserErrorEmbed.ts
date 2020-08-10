12e947a6d1de721f5fcc0897c0318ffbd656485a
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2020-08-10 13:38:37 +1000

    testing webhooks

--- src/structures/Embeds/ErrorEmbeds/UserErrorEmbed.ts
+++ src/structures/Embeds/ErrorEmbeds/UserErrorEmbed.ts
@@ -2,6 +2,7 @@ import { MessageEmbed } from "discord.js";
 export class UserErrorEmbed extends MessageEmbed {
   constructor(message: string) {
     super();
+
     this.title = `Error! ${message}`;
     this.color = 15158332;
   }
